786d33b558fc1d35dc41cf8a5a038a5d438c5166
Erick Valencia <evalencia0911@gmail.com>
2023-12-13 13:57:17 -0700

    greeting express application created

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+//import the express library
+
+const express = require("express")
+
+//create express application object
+
+const app = express()
+
+//routes
+
+app.get("/greeting/:firstname", (req, res) => {
+    const firstName = req.params.firstname
+    res.send(`<h1>Hey there, ${firstName}, how ya been!?</h1>`)
+})
+
+//turn on the server to listen
+
+app.listen(3000, () => {console.log("server is listening on port 3000")})
