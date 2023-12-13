7f53c485f7b68c3a3e5273cd6f7f49f1fb9513f1
Erick Valencia <evalencia0911@gmail.com>
2023-12-13 14:19:52 -0700

    tip calculator express application created

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get("/greeting/:firstname", (req, res) => {
     res.send(`<h1>Hey there, ${firstName}, how ya been!?</h1>`)
 })
 
+app.get("/tip/:total/:tippercentage", (req, res) => {
+    const total = req.params.total
+    const tipPercentage = req.params.tippercentage
+    res.send(`You tip should be ${total * (tipPercentage/100)}`)
+})
+
 //turn on the server to listen
 
 app.listen(3000, () => {console.log("server is listening on port 3000")})
